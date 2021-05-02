c379ad6fa2191efa00022424fcd721c1ed3f6009
FerostMask <519274834@qq.com>
2021-05-02 14:28:08 +0800

    update 5/2/2021 - 识别芯片 1. 重构找基点函数

--- 识别芯片 - 副本/CODE/CAM.c
+++ 识别芯片 - 副本/CODE/CAM.c
@@ -7,12 +7,145 @@
 #include "SEEKFREE_MT9V03X.h"
 #include "SEEKFREE_IPS200_PARALLEL8.h"
 /*--------------------------------------------------------------*/
+/* 							  宏定义 							*/
+/*==============================================================*/
+#define uint16 unsigned short
+/*--------------------------------------------------------------*/
 /* 							 变量定义 							*/
 /*==============================================================*/
 
 /*--------------------------------------------------------------*/
 /* 							 函数定义 							*/
 /*==============================================================*/
+/*------------------------------*/
+/*		  基准点辅助模块		*/
+/*==============================*/
+static void fop_search_sup(char num, unsigned char* p, char j){
+//	类型选择
+	switch(num){
+	//	左基准点判断
+		case 1:
+		//	一般情况
+			if(*p != 0xFF)
+				if(*p != 0x00){//左视场突变
+					fop_flag = 1, found_point[0] = MT9V03X_H - 1, found_point[1] = j;
+					return;
+				}
+			if(*(p+1) != 0xFF)
+				if(*(p+1) != 0x00){//右视场突变
+					fop_flag = 2, found_point[0] = MT9V03X_H - 1, found_point[1] = j+1;
+					return;
+				}
+		//	左黑右白
+			fop_flag = 3, found_point[0] = MT9V03X_H - 1, found_point[1] = j;
+			return;
+	//	右基准点判断
+		case 2:
+		//	一般情况
+			if(*p != 0xFF)
+				if(*p != 0x00){//左视场突变
+					fop_flag = 1, found_point[2] = MT9V03X_H - 1, found_point[3] = j;
+					return;
+				}
+			if(*(p+1) != 0xFF)
+				if(*(p+1) != 0x00){//右视场突变
+					fop_flag = 2, found_point[2] = MT9V03X_H - 1, found_point[3] = j+1;
+					return;
+				}
+		//	左白右黑
+			fop_flag = 3, found_point[2] = MT9V03X_H - 1, found_point[3] = j+1;
+			return;
+	//	起始点判断
+		case 0:
+		//	一般情况
+			if(*p != 0xFF)
+				if(*p != 0x00){//左起始点突变，大概率左转
+					fop_flag = 11, found_point[2] = MT9V03X_H - 1, found_point[3] = 9;
+					return;
+				}
+			if(*(p+1) != 0xFF)
+				if(*(p+1) != 0x00){//右起始点突变，大概率右转
+					fop_flag = 12, found_point[0] = MT9V03X_H - 1, found_point[1] = 10;
+					return;
+				}
+		//	特殊情况
+			if(*(uint16*)p == 0x00FF){//左白右黑的前后跳变，大概率左转
+				fop_flag = 13, found_point[2] = MT9V03X_H - 1, found_point[3] = 10;
+				return;
+			}
+			if(*(uint16*)p == 0xFF00){//左黑右白的前后跳变，大概率右转
+				fop_flag = 14, found_point[0] = MT9V03X_H - 1, found_point[1] = 9;
+				return;
+			}			
+			break;
+	}
+}
+/*------------------------------*/
+/*		  基准点寻找模块		*/
+/*==============================*/
+static void fop_search(char num){
+//	变量定义
+	register unsigned char i = MT9V03X_H - 1;
+	register char j;
+	unsigned char bina = 0, bina_temp;
+	unsigned char* p;
+//	起始点判断
+	p = &binary_img[i][9];
+	if(*(uint16*)p == 0x0000){//全黑
+	//	寻找跳变
+		p = &binary_img[i][8];
+		for(j = 8; j > 0; j--){//向左寻找，可能为左转
+			if(*(uint16*)p == 0x0000){p--;continue;}
+			fop_search_sup(2, p, j);
+			return;
+		}
+		p = &binary_img[i][10];
+		for(j = 10; j < 18; j++){
+			if(*(uint16*)p == 0x0000){p++;continue;}
+			fop_search_sup(1, p, j);
+			return;
+		}
+	//  未找到跳变，返回错误
+		fop_flag = 0;
+		return;
+	}
+	if(!bina) fop_search_sup(0, p, j);
+//	基点寻找
+	switch(num){
+		case 1:
+		//	左基点寻找 
+			p = &binary_img[i][8];
+			for(j = 8; j > -1; j--){
+				if(*(uint16*)p == 0xFFFF){p--;continue;}
+				fop_search_sup(1, p, j);
+				return;
+			}
+		//	未找到基点，向上寻找
+			j = 0;
+			for(i = MT9V03X_H - 1; i > 0; i--){
+				if(!(binary_img[i][j]^binary_img[i-1][j])) continue;
+				fop_flag = 4, found_point[0] = i, found_point[1] = j;
+				return;
+			}
+			break;
+		case 2:
+		//	右基点寻找 
+			p = &binary_img[i][10];
+			for(j = 10; j < 19; j++){
+				if(*(uint16*)p == 0xFFFF){p++;continue;}
+				fop_search_sup(2, p, j);
+				return;
+			}
+		//	未找到基点，向上寻找
+			j = 19;
+			for(i = MT9V03X_H - 1; i > 0; i--){
+				if(!(binary_img[i][j]^binary_img[i-1][j])) continue;
+				fop_flag = 4, found_point[2] = i, found_point[3] = j;
+				return;
+			}
+			break;
+	}
+}
 /*------------------------------*/
 /*		  基准点寻找模块		*/
 /*==============================*/
